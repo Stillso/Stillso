36793e23b18e13043bda4dfabe4b36d8be4ddc93
Stillso <189272@gmail.com>
2016-10-26 12:29:40 +0800

    add go top

--- footer.php
+++ footer.php
@@ -8,6 +8,13 @@
     <a href="http://www.miitbeian.gov.cn/" target="blank"> 豫ICP备16029773号-1</a>
 	</div>
 </footer>
+<script type="text/javascript" src="<?php $this->options->themeUrl('javascript/jquery.js'); ?>"></script>
+<script type="text/javascript" src="<?php $this->options->themeUrl('javascript/main.js'); ?>"></script>
+<a id="back_to_top" href="#" class="back_to_top"><span>△</span></a>
 <?php $this->footer();?>
 </body>
+
 </html>
+
+
+
